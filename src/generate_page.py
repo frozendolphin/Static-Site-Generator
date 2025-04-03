405ff67547f75b0e8d35000c46dcd0e58e86ae32
frozendolphin <122843077+frozendolphin@users.noreply.github.com>
2025-04-03 03:38:25 -0700

    now supports multiple pages

--- src/generate_page.py
+++ src/generate_page.py
@@ -1,4 +1,4 @@
-
+import os
 
 from block_markdown import markdown_to_html_node
 
@@ -29,3 +29,16 @@ def generate_page(from_path, template_path, dest_path):
     
     with open(dest_path, "w") as h:
         h.write(new_template)
+        
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+    
+    for content in os.listdir(dir_path_content):
+        new_path = os.path.join(dir_path_content, content)
+        new_dest = os.path.join(dest_dir_path, content)
+        new_dest = new_dest.replace("md", "html")
+        if os.path.isfile(new_path):
+            generate_page(new_path, template_path, new_dest)
+        if os.path.isdir(new_path):
+            if not os.path.exists(new_dest):
+                os.mkdir(new_dest)
+            generate_pages_recursive(new_path, template_path, new_dest)    
--- src/main.py
+++ src/main.py
@@ -2,12 +2,12 @@ import os
 import shutil
 
 from copy_static import delete_public_content, copy_static_to_public
-from generate_page import generate_page
+from generate_page import generate_page, generate_pages_recursive
 
 def main():
     delete_public_content()
     copy_static_to_public()
-    generate_page("content/index.md", "template.html", "public/index.html")
+    generate_pages_recursive("content/", "template.html", "public/")
     
     
 if __name__ == "__main__":
